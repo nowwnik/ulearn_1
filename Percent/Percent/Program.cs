fc8f09acabbc6e917c3272d855ae35edb5d74622
nowwnik <nik.butorin.2018@bk.ru>
2024-02-07 17:21:41 +0500

    %

---
+++ Percent/Percent/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        public static string Calculate(string userInput)
+        {
+     
+            string[] values = userInput.Split(' ');
+
+            var initialMoney = double.Parse(values[0]);
+            var yearlyPercent = double.Parse(values[1]);
+            var monthsCount = double.Parse(values[2]);
+
+            return Convert.ToString(initialMoney * Math.Pow((1 + yearlyPercent / 100 / 12), monthsCount));
+
+        }
+
+    }
+}
