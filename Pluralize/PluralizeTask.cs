b480fdf3c9f170a44ae2704c1d0094bf1bb95894
nowwnik <nik.butorin.2018@bk.ru>
2024-02-07 18:22:53 +0500

    Денешшшшки

---
+++ Pluralize/PluralizeTask.cs
@@ -0,0 +1,17 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+        if (count / 10 % 10 != 1)
+        {
+            switch (count % 10)
+            {
+                case 1: return "рубль";
+                case 2: case 3: case 4: return "рубля";
+            }
+        }
+        return "рублей";
+    }
+}
